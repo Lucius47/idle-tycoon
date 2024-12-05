3acdafe9d9c5e3dd1582b3cf5c44d20894203500
Muneeb Ullah <muhammad.muneeb@noborderz.com>
2024-12-05 18:36:05 +0500

    Persistent player cash

--- Assets/Scripts/Characters/PlayerStats.cs
+++ Assets/Scripts/Characters/PlayerStats.cs
@@ -1,5 +1,7 @@
 
+using UnityEngine;
+
 public static class PlayerStats
 {
-    public static int Cash { get; set; }
+    public static int Cash { get { return PlayerPrefs.GetInt("Cash", 0); } set { PlayerPrefs.SetInt("Cash", value); } }
 }
